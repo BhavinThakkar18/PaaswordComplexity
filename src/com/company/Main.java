5271d574a5322717a2d52af16d697a8dddeb27ec
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-18 15:39:23 +0530

    added normalizedString method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
     public static boolean isPsswordComplex(String s)
     {
@@ -8,8 +10,19 @@ public class Main {
         //atleast one Digit
         return  (s.chars().anyMatch(Character::isUpperCase) && s.chars().anyMatch(Character::isLowerCase) && s.chars().anyMatch(Character::isDigit));
     }
+    public static String normalizeString(String s)
+    {
+        //Remove extra spaces
+        //convert all characters in lowercase
+        //replace , with space if it occures in  the string
+        return (s.toLowerCase().trim().replace(",",""));
+//        String lowercased=s.toUpperCase();
+//        String trimmed=lowercased.trim();
+//        String normalized = trimmed.replace(",",,"");
+    }
     public static void main(String[] args) {
         System.out.println(isPsswordComplex("Abc@123"));
         System.out.println(isPsswordComplex("abc@123"));
+        System.out.println(normalizeString("  Hello There, Buddy!"));
     }
 }
